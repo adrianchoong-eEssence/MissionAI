11d6f94b9f022b7ea60d265bc6957012a87be903
adrianchoong-eEssence <adrian.choong@gmail.com>
2026-07-09 10:38:22 +0800

    Fix google drive syntax error

--- data/google_drive.py
+++ data/google_drive.py
@@ -121,7 +121,8 @@ def upload_photo(event_id, mission_id, team_name, participant_name, uploaded_fil
         "file_id": file_id,
         "url": file.get("webViewLink", ""),
         "filename": filename,
-    }    def get_team_submission(self, event_id, mission_id, team_name):
+                 }
+    def get_team_submission(self, event_id, mission_id, team_name):
         rows = self.submissions.get_all_records()
 
         for row in rows:
